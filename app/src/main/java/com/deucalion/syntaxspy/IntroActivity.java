dc7d1bf467d370a643519cc38ad5dacdec7d8de8
AbhijeetKumar1112 <abhijeetdelhi1112@gmail.com>
2023-10-12 02:31:25 +0530

    adding app icon

---
+++ app/src/main/java/com/deucalion/syntaxspy/IntroActivity.java
@@ -0,0 +1,26 @@
+package com.deucalion.syntaxspy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent iHome = new Intent(IntroActivity.this, MainActivity.class);
+                startActivity(iHome);
+                finish();
+            }
+        }, 10000);
+    }
+
+}
