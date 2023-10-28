40f50715d6e1c2253a354cddd82bf0b61f22e30d
CodeThyDominus <thydominus@gmail.com>
2023-10-28 13:55:53 +0530

    creating settings activity

--- app/src/main/java/com/deucalion/syntaxspy/Setting.java
+++ app/src/main/java/com/deucalion/syntaxspy/Setting.java
@@ -1,14 +1,30 @@
 package com.deucalion.syntaxspy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Setting extends AppCompatActivity {
+
+    Button changeFontButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        findId();
     }
+
+    public void setChangeFontButton(Button changeFontButton) {
+        this.changeFontButton = changeFontButton;
+    }
+
+    public void findId(){
+        changeFontButton = findViewById(R.id.changeFontButton)
+    }
+
 }
+
